651eea99d980fe020a2f73414c1a5a75d55f449c
nandiprasadkm18 <nandiprasadkm18@gmail.com>
2025-11-07 22:03:07 +0530

    Update vitconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CampusConnect/', // 👈 this line is the fix
 })
